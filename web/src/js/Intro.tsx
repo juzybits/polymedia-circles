44ae6c309918d3c854ad5812b1d233351e7d0db3
juzybits <96890594+juzybits@users.noreply.github.com>
2023-06-07 22:17:15 +0000

    [web] Intro: 'How does it work?' and 'How is the artwork dynamic?'

--- web/src/js/Intro.tsx
+++ web/src/js/Intro.tsx
@@ -6,13 +6,13 @@ export const Intro: React.FC = () =>
     <div id='intro'>
         <div id='title'>
             <h1>Circles.</h1>
-            <h2>by Polymedia</h2>
+            <h2>by <a id='by_polymedia' href='https://polymedia.app/' target='_blank'>Polymedia</a></h2>
         </div>
         <div id='faq'>
             <h1>F.A.Q.</h1>
 
             <div id='note'>
-                * Last update: June 5, 2023
+                * Last update: June 7, 2023
                 <br/>
                 * This living document is subject to change.
             </div>
@@ -21,7 +21,7 @@ export const Intro: React.FC = () =>
             <div className='question'>
                 <h2>What is Polymedia Circles?</h2>
                 <p>Polymedia Circles is a generative art project built on Sui.</p>
-                <p>From a certain point of view, it is an NFT collection.</p>
+                <p>From one point of view, it is a series of digital collectables.</p>
                 <p>From another point of view, it is code that generates visual art.</p>
             </div>
 
@@ -36,14 +36,29 @@ export const Intro: React.FC = () =>
 
                     <p>2. The artwork should be stored fully on-chain. No links to external storage like AWS or IPFS are allowed. Therefore, the artwork size (in bytes) must be small in order to reduce Sui storage fees and to stay within the object size limit.</p>
 
-                    <p>3. The artwork should display natively in Sui wallets and explorers. No additional code or libraries are allowed. Therefore, the artwork must be compatible with the Sui <Link text='Display Standard' href='https://docs.sui.io/build/sui-object-display' /> so it shows everywhere out of the box.</p>
+                    <p>3. The artwork should display natively in Sui wallets and explorers. No additional code or libraries are allowed. Therefore, the artwork must be compatible with the Sui <Link text='Display Standard' href='https://docs.sui.io/build/sui-object-display' /> so that it works everywhere out of the box.</p>
 
-                    <p>4. The artwork should be dynamic. Owners will have the ability to modify it according to certain rules. This introduces a new element of creative unpredictability: not only are the initial artworks generated algorithmically, but subsequent versions will depend on the choices of their owners. More details on this later.</p>
+                    <p>4. The artwork should be dynamic. Owners will have the ability to modify it according to certain rules. This introduces a new element of creative unpredictability: not only are the initial artworks generated algorithmically, but subsequent versions will depend on the choices of their owners.</p>
             </div>
 
             <div className='question'>
                 <h2>How does it work?</h2>
-                <p>Using an algorithm and randomness, an <Link text='SVG' href='https://developer.mozilla.org/en-US/docs/Web/SVG' /> is built on-chain when someone sends a mint transaction. The SVG is then formatted as a <Link text='data URL' href='https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/Data_URLs' /> in the artwork's <Link text='Display' href='https://docs.sui.io/build/sui-object-display' />, so that it renders an image in wallets and explorers.</p>
+                <p>When someone sends a mint transaction, an <Link text='SVG' href='https://developer.mozilla.org/en-US/docs/Web/SVG' /> is built on-chain using the Circles algorithm sprinkled with randomness. The SVG is then formatted as a <Link text='data URL' href='https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/Data_URLs' /> in the artwork's <Link text='Display' href='https://docs.sui.io/build/sui-object-display' />, so that it renders an image in wallets and explorers.</p>
+                <p>Each artwork consists of a background and between 2 and 5 circles. The background and circle colors are random, within a predefined palette of 216 colors. The circle position and size are also random, within the limits of the canvas. Smaller circles are rendered on top, so that every circle is visible.</p>
+                <p>Each artwork is represented on-chain as a Sui object. It not only stores its own SVG representation, which allows it to be displayed as an image, but also the properties of its components, such as the size and color of each circle. This is what enables the dynamic nature of Polymedia Circles.</p>
+            </div>
+
+            <div className='question'>
+                <h2>How is the artwork dynamic?</h2>
+                <p>At this time I have implemented two dynamic features, but many more are possible and may be added in the future. I believe these two simple features are enough to introduce an element of human selection on top of the algorithmic randomness which will lead to more aesthetic and interesting outcomes.</p>
+                <p>1) Recycling artwork: holders can destroy existing artwork objects in order to mint new artwork at a big discount. Because aspects of Polymedia Circles are random, some pieces will be more aesthetic than others. If a minter is not happy with the artwork they got, they can recycle it into a new one for just 10% of the full price.</p>
+                <p>2) Blending artwork: sometimes an artwork is almost perfect... if only you make one change! Well, Polymedia Circles lets holders combine two artworks by picking which individual circles they'd like to swap. This means holders can refine their artwork according to their individual taste while shaping the collection in the process.</p>
+            </div>
+
+            <div className='question'>
+                <h2>What kind of ownership do holders have?</h2>
+                <p>From a copyright perspective, you are free to use your artwork however you like, even commercially. The exact license is yet to be decided, but I am leaning towards <Link text='CC BY-SA' href='https://creativecommons.org/licenses/by-sa/4.0/'/>.</p>
+                <p>From a technical perspective, artworks are "owned" objects, which are completely controlled by their holders. There is no "shared" object wrapper that limits what you can do with your artwork.</p>
             </div>
 
             <div className='question'>
@@ -51,7 +66,7 @@ export const Intro: React.FC = () =>
                 <p>Polymedia Circles has a unique minting mechanism. Instead of having a fixed mint price, each artwork will be slightly more expensive than the previous one. This means that the mint price will increase exponentially.
                 </p>
                 <p>
-                    The exact initial mint price and growth factor are yet to be decided. But as an example, if the initial price is 1 SUI and the price increments by 0.1% on each mint, then:
+                    The exact initial mint price and growth factor are yet to be decided, but my idea is to start very cheap. As an example, if the initial price is 1 SUI and the price increments by 0.1% on each mint, then:
                 </p>
                 <p>
                 - The 1st artwork costs 1.00 SUI<br/>
@@ -66,12 +81,17 @@ export const Intro: React.FC = () =>
 
             <div className='question'>
                 <h2>What is the total supply?</h2>
-                <p>While technically there is no supply limit, in practice the increasing minting price (see previous question) will limit supply to most likely just a few hundred artworks.</p>
+                <p>While technically there is no supply limit, in practice the increasing mint price (see previous question) will limit supply to most likely just a few hundred artworks.</p>
             </div>
 
             <div className='question'>
-                <h2>Is there a whitelist?</h2>
-                <p>There can be a whitelist if there is interest. If you are a Polymedia supporter and want to be included, please reach out on Twitter. If I (@juzybits) follow you, or we've interacted in the past, there's a good chance you are eligible for the whitelist.</p>
+                <h2>Are there any royalties?</h2>
+                <p>Provided I implement artwork ownership in the manner I described in <i>"What kind of ownership do holders have?</i>, royalties will not be enforceable.</p>
+            </div>
+
+            <div className='question'>
+                <h2>Where will it trade?</h2>
+                <p>Any venues that want to enable trading of Polymedia Circles are free to do so, as I will not be placing any constraints on the collection.</p>
             </div>
 
             <div className='question'>
@@ -80,19 +100,8 @@ export const Intro: React.FC = () =>
             </div>
 
             <div className='question'>
-                <h2>What kind of ownership do holders have?</h2>
-                <p>From a copyright perspective, you are free to use your artwork however you like, even commercially. The exact license is yet to be decided, but I am leaning towards <Link text='CC BY-SA' href='https://creativecommons.org/licenses/by-sa/4.0/'/>.</p>
-                <p>From a technical perspective, artworks are "owned" objects, which are completely controlled by their holders. There is no "shared" object wrapper that limits what you can do with your artwork.</p>
-            </div>
-
-            <div className='question'>
-                <h2>Are there any royalties?</h2>
-                <p>Provided I implement artwork ownership in the manner I described in the previous question, royalties will not be enforceable.</p>
-            </div>
-
-            <div className='question'>
-                <h2>Where will it trade?</h2>
-                <p>Any NFT venues that want to enable trading of Polymedia Circles are free to do so, as I will not be placing any constraints on the collection.</p>
+                <h2>Is there a whitelist?</h2>
+                <p>There can be a whitelist if there is interest. If you are a Polymedia supporter and want to be included, please reach out on Twitter. If I (<Link text='@juzybits' href='https://twitter.com/juzybits' />) follow you, or we've had a positive interaction in the past, there's a good chance you are eligible for the whitelist.</p>
             </div>
 
             <div className='question'>
