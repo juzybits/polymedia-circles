60ca35faa07493dac93af3811059756268e3a403
juzybits <96890594+juzybits@users.noreply.github.com>
2023-11-06 20:37:30 -0100

    [web] delete old .tsx and .less

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,19 +1,10 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AppWrap } from './js/App';
-// Main pages
+
 import { Home } from './js/Home';
 import { FAQ } from './js/FAQ';
-import { Activity } from './js/Activity';
-import { MyCircles } from './js/MyCircles';
-// Artwork actions
 import { Mint } from './js/Mint';
-import { View } from './js/View';
-import { Freeze } from './js/Freeze';
-import { Burn } from './js/Burn';
-import { Recycle } from './js/Recycle';
-import { Blend } from './js/Blend';
-// Special pages
 import { Demo } from './js/Demo';
 import { NotFound } from './js/NotFound';
 
@@ -23,19 +14,9 @@ ReactDOM
         <BrowserRouter>
         <Routes>
             <Route path='/' element={<AppWrap />} >
-                {/* main pages */}
                 <Route index element={<Home />} />
                 <Route path='faq' element={<FAQ />} />
-                <Route path='activity' element={<Activity />} />
-                <Route path='mycircles' element={<MyCircles />} />
-                {/* artwork actions */}
                 <Route path='mint' element={<Mint />} />
-                <Route path='view/:artId' element={<View />} />
-                <Route path='freeze/:artId' element={<Freeze />} />
-                <Route path='burn/:artId' element={<Burn />} />
-                <Route path='recycle/:artId' element={<Recycle />} />
-                <Route path='blend/:artId' element={<Blend />} />
-                {/* Special pages */}
                 <Route path='demo' element={<Demo />} />
                 <Route path='*' element={<NotFound />} />
             </Route>
--- web/src/js/NotFound.tsx
+++ web/src/js/NotFound.tsx
@@ -1,15 +1,9 @@
-import { useEffect } from 'react';
-
 export function NotFound()
 {
-    useEffect(() => {
-        document.title = 'Polymedia Circles - Not found';
-    }, []);
-
     return <>
         <h1>404</h1>
         <div>
-            Not found.
+            Page not found.
         </div>
     </>;
 }
