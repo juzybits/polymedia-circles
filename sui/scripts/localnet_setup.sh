8f7ce3407dcc6830034dc4f861c89a6a518a0b03
juzybits <96890594+juzybits@users.noreply.github.com>
2023-11-12 02:54:50 -0100

    [sui] modify scripts/localnet_setup.sh

--- sui/scripts/localnet_setup.sh
+++ sui/scripts/localnet_setup.sh
@@ -1,18 +1,27 @@
 #!/usr/bin/env bash
 
+set -ueo pipefail
+
 shopt -s expand_aliases
 source ~/.bash_aliases
 source ~/.bash_functions
 
-echo "1) ----- Starting | $(sc active-env) | $(sc active-address)"
+echo -e "\n\n1) ----- Starting | $(sc active-env) | $(sc active-address) -----"
 
-echo "2) ----- Sending 10 faucets to active-address"
-loop 10 'zuifaucet $(sc active-address)'
+echo -e "\n2) ----- Sending SUI to active-address and dev browser wallet -----\n"
+zuifaucet $(sc active-address) | cut  -c 1-63
+zuifaucet 0x5d8133281aa26ad73542c0b53014c6831c37b9d98e7603fd0db2e1cc4453934a | cut  -c 1-63
 
-echo "3) ----- Merging all gas coins"
-sc pay-all-sui --gas-budget 1000000000 --recipient $(sc active-address) --input-coins $(sc gas | tail -n +3 | awk '{print $1}' | tr '\n' ' ')
+echo -e "\n3) ----- Publishing package -----\n"
+sui client publish --gas-budget 100000000
 
-echo "4) ----- Splitting gas coin"
-sc pay-sui --gas-budget 1000000000 --recipients $(sc active-address) --amounts 5000000000 --input-coins $(sc gas | grep 0x | awk '{print $1}')
+echo -e "\n4) ----- Opening Move.toml and circles.ts -----\n"
+code ./Move.toml ../web/src/js/lib/circles.ts
+
+# echo "3) ----- Merging all gas coins"
+# sc pay-all-sui --gas-budget 1000000000 --recipient $(sc active-address) --input-coins $(sc gas | tail -n +3 | awk '{print $1}' | tr '\n' ' ')
+
+# echo "4) ----- Splitting gas coin"
+# sc pay-sui --gas-budget 1000000000 --recipients $(sc active-address) --amounts 5000000000 --input-coins $(sc gas | grep 0x | awk '{print $1}')
 
 echo "----- Done"
