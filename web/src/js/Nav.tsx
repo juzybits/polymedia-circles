6e68b188f80ab39a3945dbdd36da0cf1d029aa0a
juzybits <96890594+juzybits@users.noreply.github.com>
2023-11-12 02:09:41 -0100

    [web] #nav-pages (basic)

--- web/src/js/Nav.tsx
+++ web/src/js/Nav.tsx
@@ -1,6 +1,7 @@
 import { useWalletKit } from '@mysten/wallet-kit';
 import { NetworkSelector } from '@polymedia/react-components';
 import { NetworkName, shortenAddress } from '@polymedia/webutils';
+import { Link } from 'react-router-dom';
 import '../css/Nav.less';
 import { isDev } from './lib/isDev';
 
@@ -19,21 +20,35 @@ export const Nav: React.FC<{
     const showNetworkSelector = isDev();
 
     return (
-        <header id='nav'>
+    <header id='nav'>
         <div id='nav-user' className='nav-section'>
         {
-            !currentAccount
-            ?
+        !currentAccount ?
             <span id='nav-btn-connect' onClick={openConnectModal}>
                 LOG IN
             </span>
-            :
+        :
             <span onClick={disconnect}>
                 Logged in as {shortenAddress(currentAccount.address)}
             </span>
         }
-        {showNetworkSelector && <NetworkSelector currentNetwork={network} />}
+            {showNetworkSelector && <NetworkSelector currentNetwork={network} />}
         </div>
-        </header>
+
+        <div id='nav-pages' className='nav-section'>
+            <div className='nav-page-link'>
+                <Link to='/'>HOME</Link>
+            </div>
+            <div className='nav-page-link'>
+                <Link to='/mint'>MINT</Link>
+            </div>
+            <div className='nav-page-link'>
+                <Link to='/owner'>OWNER</Link>
+            </div>
+            <div className='nav-page-link'>
+                <Link to='/faq'>FAQ</Link>
+            </div>
+        </div>
+    </header>
     );
 }
